a53be7f3bf53d8c9b2cfb2ac36c1d72ee4b4b5ae
root <root@LAPTOP-SK9NHHRQ.>
2024-11-15 20:52:50 +0800

    Move build config function to common lib

--- src/lib.rs
+++ src/lib.rs
@@ -56,3 +56,30 @@ fn write_encoded_data_to_bin_file(binary_data: &Vec<u8>, file: &str) -> io::Resu
     Ok(())
 }
 
+
+pub fn build_config_template(filename: &str) -> Result<()>{
+    let mut file = std::fs::File::create(filename)?;
+    let default_content = r#"
+Chassis_type = "Rack Mount Chassis"
+Chassis_Part_Number = "CHS1234"
+Chassis_Serial_Number = "SN5678"
+Chassis_Extra = "Chassis extra"
+Board_Manufacturer = "qwfqwfg"
+Board_Product_Name = "Board124"
+Board_Serial_Number = "SN12345"
+Board_Part_Number = "BP9876"
+Board_Fruid = "FRU123"
+Board_Extra = "Board extra"
+Product_Manufacturer = "ProductMFC"
+Product_Name = "Product1"
+Product_Part_Number = "PN5678"
+Product_Version = "V1.0.0"
+Product_Serial_Number = "SN123456"
+Product_Asset_Tag = "AssetTag"
+Product_Extra = "Product extra"
+"#;
+
+    file.write_all(default_content.as_bytes())?;
+    Ok(())
+}
+
